3c183c7e371979c456676c8ecd47f295a3948cb1
Felix Klotzsche <klotzsche@cbs.mpg.de>
2024-03-01 17:17:24 +0100

    add marker funcs, rm time sync stuff

--- Assets/edia.lsl/Runtime/Assets/Dev/DevMarkerSender.cs
+++ Assets/edia.lsl/Runtime/Assets/Dev/DevMarkerSender.cs
@@ -8,8 +8,10 @@ public class DevMarkerSender : MonoBehaviour
     public MarkerOutlet MarkerOutletTest;
 
 	private void Start() {
-        StartCoroutine(MarkerSenderTypeOne());
-		StartCoroutine(MarkerSenderTypeTwo());
+        // StartCoroutine(MarkerSenderTypeOne());
+		// StartCoroutine(MarkerSenderTypeTwo());
+		StartCoroutine(MarkerSenderTypeThree());
+
 	}
 
 	IEnumerator MarkerSenderTypeOne() {
@@ -27,4 +29,15 @@ public class DevMarkerSender : MonoBehaviour
 			MarkerOutletTest.SendMarker("Stimuli Created");
 		}
 	}
+
+	IEnumerator MarkerSenderTypeThree() {
+	while (true) {
+            yield return new WaitForSecondsRealtime(Random.Range(1f, 3f));
+			MarkerOutletTest.SendMarker("Marker sent without specifications");
+            MarkerOutletTest.SendMarker("Marker Sent Now", MarkerOutlet.MomentForMarker.Now);
+			MarkerOutletTest.SendMarker("Marker Sent EndOfFrame", MarkerOutlet.MomentForMarker.EndOfFrame);
+			MarkerOutletTest.SendMarker("Marker Sent Start Of Next Frame", MarkerOutlet.MomentForMarker.StartOfFrame);
+			MarkerOutletTest.SendMarker("Marker Sent With 3 Frames Delay", MarkerOutlet.MomentForMarker.StartOfFrame, 3);
+        }
+    }
 }
--- Assets/edia.lsl/Runtime/Assets/Scripts/MarkerOutlet.cs
+++ Assets/edia.lsl/Runtime/Assets/Scripts/MarkerOutlet.cs
@@ -8,10 +8,11 @@ namespace edia.lsl {
  
     public class MarkerOutlet : AStringOutlet
     {
-        public float ResetInterval = 3.4f;
-        private float elapsed_time = 0.0f;
-        private Material mat;
-		private bool isBuildSample = false;
+
+        private Queue<string> _triggersQueued = new();
+        private bool _isBuildSample = false;
+
+        public enum MomentForMarker { StartOfFrame, EndOfFrame, Now }
 
 		public override List<string> ChannelNames
         {
@@ -22,6 +23,7 @@ namespace edia.lsl {
             }
         }
 
+
         public void Reset()
         {
             StreamName = "Unity.Event";
@@ -29,21 +31,127 @@ namespace edia.lsl {
             moment = MomentForSampling.EndOfFrame;
             IrregularRate = true;
         }
+
+
+        protected override void Update() {
+            if (_triggersQueued.Count > 0) {
+                int lengthQueue = _triggersQueued.Count;
+                for (int i = 0; i < lengthQueue; i++) {
+                    sample[0] = _triggersQueued.Dequeue();
+                    pushSample();
+                }
+            }
+
+            base.Update();
+
+        }
+
+
         /// <summary>
         /// Send a marker over the configured LSL stream.
         /// </summary>
         /// <param name="value">String value for marker</param>
         public void SendMarker (string value) {
 			sample[0] = value;
-            isBuildSample = true; // Trigger for actually sending sample in BuildSample method
+            _isBuildSample = true; // Trigger for actually sending sample in BuildSample method
         }
 
+
+        /// <summary>
+        /// Sends a marker immediately, at the end of the current frame, or at the start of the next frame based on the specified moment.
+        /// Note that this will behave slightly differently than SendMarker(marker) without further arguments: with arguments it will ignore 
+        /// a TimeSync component and just timestamp the marker with the current LSL time. It will also overrule the moment (MomentForSampling)
+        /// which may have been set in the editor.
+        /// </summary>
+        /// <param name="marker">The marker to be sent.</param>
+        /// <param name="moment">The moment during the frame to send the marker, 
+        /// which can be immediately (Now), at the end of the current frame (EndOfFrame), 
+        /// or at the start of the next frame (StartOfFrame), Defaults to Now.</param>
+        public void SendMarker(string marker, MomentForMarker moment = MomentForMarker.Now) {
+            switch (moment) {
+                case MomentForMarker.StartOfFrame:
+                    StartCoroutine(sendMarkerInXFrames(marker, 0));
+                    break;
+                case MomentForMarker.EndOfFrame:
+                    sendMarkerAtEndOfFrame(marker);
+                    break;
+                case MomentForMarker.Now:
+                    pushMarker(marker);
+                    break;
+            }
+        }
+
+
+        /// <summary>
+        /// Sends a marker with an optional delay. 
+        /// This version allows specifying a delay in frames, but currently only supports sending 
+        /// at the start of a frame if there's a delay.
+        /// A delay of 0 means the marker will be sent asap in the next frame.
+        /// </summary>
+        /// <param name="marker">The marker to be sent.</param>
+        /// <param name="moment">The moment during the frame to send the marker. Defaults to StartOfFrame. 
+        /// Currently, delayed markers can only be sent at the start of a frame.</param>
+        /// <param name="nFramesDelay">The number of frames to delay before sending the marker. 
+        /// Must be non-negative. Defaults to 0, meaning marker is sent asap in the next frame.</param>
+        public void SendMarker(string marker, MomentForMarker moment = MomentForMarker.StartOfFrame, int nFramesDelay = 0) {
+            if (nFramesDelay < 0) {
+                Debug.LogError("nFramesDelay must be greater than or equal to 0");
+            }
+
+            if (moment != MomentForMarker.StartOfFrame) {
+                Debug.LogError("Delayed markers can only be sent at the beginning of a frame for now.");
+            }
+
+            if (nFramesDelay >= 0) {
+                StartCoroutine(sendMarkerInXFrames(marker, nFramesDelay));
+            }
+        }
+
+
         protected override bool BuildSample() {
-            if (isBuildSample) { isBuildSample = false;
+            if (_isBuildSample) { 
+                _isBuildSample = false;
                 return true;
             }
 
             return false;
+        }
+
+
+        public void sendMarkerAtEndOfFrame(string marker) {
+            sample[0] = marker;
+            StartCoroutine(pushMarkerAtEndOfFrame(marker));
+        }
+
+        
+        public void sendMarkerNextFrame(string trigger) {
+            if (_triggersQueued == null) {
+                _triggersQueued = new();
+            }
+            _triggersQueued.Enqueue(trigger);
+        }
+
+
+        void pushMarker(string marker) {
+            sample[0] = marker;
+            pushSample();
+        }
+
+
+        IEnumerator pushMarkerAtEndOfFrame(string marker) {
+            yield return new WaitForEndOfFrame();
+            pushMarker(marker);
+        }
+
+
+        IEnumerator sendMarkerInXFrames(string trigger, int frames) {
+            for (int i = 0; i < frames - 1; i++) {
+                yield return null;
+            }
+            if (_triggersQueued == null) {
+                _triggersQueued = new();
+            }
+            _triggersQueued.Enqueue(trigger);
         }
     }
 }
