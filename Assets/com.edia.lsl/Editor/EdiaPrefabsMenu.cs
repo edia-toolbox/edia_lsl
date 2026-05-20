33d1445c3299348261d0250cae56339eac06b67f
eioe <klotzsche@cbs.mpg.de>
2026-05-20 17:10:45 +0200

    fix menu script; mv prefabs out of Resources

--- Assets/com.edia.lsl/Editor/EdiaPrefabsMenu.cs
+++ Assets/com.edia.lsl/Editor/EdiaPrefabsMenu.cs
@@ -2,36 +2,52 @@ using UnityEngine;
 using UnityEditor;
 
 namespace Edia.Lsl.Editor {
-    
-}
 
-public class EdiaPrefabsMenu : MonoBehaviour
-{
-    [MenuItem("GameObject/EDIA/LSL/Extended-PositionRotationOutlet", false, 0)]
-    private static void CreatePrefabExtendedPosRotOutlet(MenuCommand menuCommand) { 
-        InstantiatePrefab("LSL-Extended-PositionRotationOutlet", menuCommand);
-    }
-    
-    [MenuItem("GameObject/EDIA/LSL/MarkerOutlet", false, 0)]
-    private static void CreatePrefabMarkerOutlet(MenuCommand menuCommand) { 
-        InstantiatePrefab("LSL-MarkerOutlet", menuCommand);
-    }
-    
-    [MenuItem("GameObject/EDIA/LSL/CustomFloatOutlet", false, 0)]
-    private static void CreatePrefabCustomFloatOutlet(MenuCommand menuCommand) { 
-        InstantiatePrefab("LSL-CustomFloatOutlet", menuCommand);
-    }
-    
-    private static void InstantiatePrefab(string prefabName, MenuCommand command) {
-        var prefab = Resources.Load<GameObject>($"{prefabName}");
-        if (prefab == null) {
-            Debug.LogError($"Prefab '{prefabName}' not found in Resources");
-            return;
+    public static class EdiaPrefabsMenu {
+        [MenuItem("GameObject/EDIA/LSL/Extended-PositionRotationOutlet", false, 0)]
+        private static void CreatePrefabExtendedPosRotOutlet(MenuCommand menuCommand) {
+            InstantiatePrefab("LSL-Extended-PositionRotationOutlet", menuCommand);
         }
 
-        var instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
-        GameObjectUtility.SetParentAndAlign(instance, command.context as GameObject);
-        Undo.RegisterCreatedObjectUndo(instance, $"Create {prefabName}");
-        Selection.activeObject = instance;
+        [MenuItem("GameObject/EDIA/LSL/MarkerOutlet", false, 0)]
+        private static void CreatePrefabMarkerOutlet(MenuCommand menuCommand) {
+            InstantiatePrefab("LSL-MarkerOutlet", menuCommand);
+        }
+
+        [MenuItem("GameObject/EDIA/LSL/CustomFloatOutlet", false, 0)]
+        private static void CreatePrefabCustomFloatOutlet(MenuCommand menuCommand) {
+            InstantiatePrefab("LSL-CustomFloatOutlet", menuCommand);
+        }
+
+        private static void InstantiatePrefab(string prefabName, MenuCommand command) {
+
+            string[] prefabBasePaths = {
+                "Packages/com.edia.lsl/Runtime/Prefabs/",
+                "Assets/com.edia.lsl/Runtime/Prefabs/"
+            };
+
+            GameObject prefab = null;
+            string failedPaths = "";
+
+            foreach (var prefabPath in prefabBasePaths) {
+                string fullPath = $"{prefabPath}{prefabName}.prefab";
+                prefab = AssetDatabase.LoadAssetAtPath<GameObject>(fullPath);
+                if (prefab != null) {
+                    break;
+                }
+                failedPaths += $" - {prefabPath}\n";
+            }
+
+            if (prefab == null) {
+                Debug.LogError($"Prefab '{prefabName}' not found. Tried:\n{failedPaths}");
+                return;
+            }
+
+            var instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+            GameObjectUtility.SetParentAndAlign(instance, command.context as GameObject);
+            Undo.RegisterCreatedObjectUndo(instance, $"Create {prefabName}");
+            Selection.activeObject = instance;
+        }
     }
+
 }
